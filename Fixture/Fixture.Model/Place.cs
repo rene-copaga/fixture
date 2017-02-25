bbe4842d429476f7801c7727cf3de861d55b0017
rene_copaga <rene.copaga@gmail.com>
2017-02-25 17:31:29 -0400

    Basic classes for modeling a championship.

---
+++ Fixture/Fixture.Model/Place.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Fixture.Model
+{
+    public class Place
+    {
+        string placeName;
+        string address;
+
+        public Place(string placeName, string address)
+        {
+            if (placeName == null)
+            {
+                throw new ArgumentNullException("placeName");
+            }
+
+            if (address== null)
+            {
+                throw new ArgumentNullException("address");
+            }
+
+            this.placeName = placeName;
+            this.address   = address;
+        }
+
+        public string PlaceName
+        {
+            get { return placeName; }
+            set { placeName = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+    }
+}
---
+++ Fixture/Fixture.Model/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Fixture.Model
+{
+    public class Player : Participant
+    {
+        private string names;
+        private string lastNames;
+        private int height;
+        private int weight;
+        DateTime birthDate;
+
+        public Player(string names, string lastNames, DateTime birthDate) : base()
+        {
+            this.names = names;
+            this.lastNames = lastNames;
+            this.birthDate = birthDate;
+        }
+
+        public string Names
+        {
+            get { return names; }
+            set { names = value; }
+        }
+
+        public string LastNames
+        {
+            get { return lastNames; }
+            set { lastNames = value; }
+        }
+
+        public DateTime BirthDate
+        {
+            get { return birthDate; }
+            set { birthDate = value; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        public int Weight
+        {
+            get { return weight; }
+            set { weight = value; }
+        }
+    }
+}
